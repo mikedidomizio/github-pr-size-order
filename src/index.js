734879d6f7528423484246f87283470907c5ecc9
Mike DiDomizio <mikedidomizio@gmail.com>
2021-11-13 13:29:26 -0500

    appropriate rename of variable

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const { Octokit, App } = require("octokit");
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
 (async() => {
-    const response = await octokit.rest.pulls.list({
+    const listOfPullRequests = await octokit.rest.pulls.list({
         owner: 'mikedidomizio',
         repo: 'github-pr-size-order'
     })
 
-    console.log(response);
+    console.log(listOfPullRequests);
 })();
